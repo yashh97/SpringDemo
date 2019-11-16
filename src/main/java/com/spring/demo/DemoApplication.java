e81f937f34463e9d27d01ecaa48ca0af1157996f
Yash <yashrajmishra2509@gmail.com>
2019-11-17 00:25:23 +0530

    Final

--- src/main/java/com/spring/demo/DemoApplication.java
+++ src/main/java/com/spring/demo/DemoApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 kjmpdAS ASDSFDSzbfbc   MOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo
 @SpringBootApplk;klm;lm;kljm;;lication
+import lombok.*
+
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-			maaaaa kiii chotttttttttt
+			maaaaa kiii chooooooooooooooooooooooooooooooootttttttttt
 	}
 
 }
